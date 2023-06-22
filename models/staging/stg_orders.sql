88a61aebcd684652e068f3472bd05ac688d197af
wjmcleod98@gmail.com <wjmcleod98@gmail.com>
2023-06-22 19:20:06 +0000

    Developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,28 @@
+/*Can reference another model in dbt rather than having to hard pull from the database by using __ref.
+I'm not sure, but maybe this is useful because it could be faster?*/
+
+/*Can run the model and all the models downstream of the model by doing -s +'modelname'*/
+
+select
+-- from raw_orders
+o.customerid,
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+o.ordercostprice,
+o.ordersellingprice,
+-- from raw_customer
+c.customername,
+c.segment,
+c.country,
+-- from raw_product
+p.category,
+p.productname,
+p.subcategory
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+    on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+    on o.productid = p.productid
