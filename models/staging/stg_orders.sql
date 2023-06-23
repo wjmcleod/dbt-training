767f6fd3b944b874f60fd68ee356b88ed514418b
wjmcleod98@gmail.com <wjmcleod98@gmail.com>
2023-06-23 17:58:36 +0000

    Not sure what's different here but it all works the same so whatever

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -23,9 +23,12 @@ p.productid,
 p.category,
 p.productname,
 p.subcategory,
-{{markup('o.ordersellingprice', 'o.ordercostprice')}} as markup
+{{markup('o.ordersellingprice', 'o.ordercostprice')}} as markup,
+d.delivery_team
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
     on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
     on o.productid = p.productid
+left join {{ ref('delivery_team') }} as d
+    on o.shipmode = d.shipmode
