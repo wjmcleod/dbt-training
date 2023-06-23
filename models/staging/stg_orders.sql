d1852d24d6c483be40bc092c1bdec8c91b058c64
wjmcleod98@gmail.com <wjmcleod98@gmail.com>
2023-06-23 17:30:35 +0000

    Added macros

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -21,7 +21,8 @@ c.country,
 p.productid,
 p.category,
 p.productname,
-p.subcategory
+p.subcategory,
+{{markup('o.ordersellingprice', 'o.ordercostprice')}} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c
     on o.customerid = c.customerid
